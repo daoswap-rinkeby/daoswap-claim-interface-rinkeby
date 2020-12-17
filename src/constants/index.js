298433b291fe204b50998bcf9ae58d5aa366c889
daoswap001 <daoswap@daoswap.global>
2020-12-17 17:02:17 +0800

    alert

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+import ERC20_CONTRACT from './contracts/ERC20FixedSupply.json';
+import CLAIM_CONTRACT from './contracts/Claim.json';
+
+export { ERC20_CONTRACT, CLAIM_CONTRACT }
+
+export const ERC20ContractAddress = '0x29Ac4ed3Ad8457B6Fb75B336D688C9094AD08B4b';
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="react-scripts" />
 
+declare module 'fortmatic';
+
 interface Window {
   ethereum?: {
     isMetaMask?: true;
