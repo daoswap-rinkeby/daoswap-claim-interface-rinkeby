eced08f815d2471dad204f00af8c8e65ee9c9385
daoswap001 <daoswap@daoswap.global>
2021-01-08 17:55:05 +0800

    alert

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,17 +3,20 @@ import CLAIM_CONTRACT from "./contracts/Claim.json";
 
 export { ERC20_CONTRACT, CLAIM_CONTRACT };
 
-export const ERC20ContractAddress =
-  "0x29Ac4ed3Ad8457B6Fb75B336D688C9094AD08B4b";
-
 // export const ERC20ContractAddress =
-//   "0xdb5D970F03bfD19c1E51D57BcEd114BC35A0808f";
+//   "0x29Ac4ed3Ad8457B6Fb75B336D688C9094AD08B4b";
+
+export const ERC20ContractAddress =
+  "0xdb5D970F03bfD19c1E51D57BcEd114BC35A0808f";
+
+export const ClaimContractAddress =
+  "0xb0EB94837d5Bc7AbB863Ec729e05E2A6252Ae77a";
 
 // export const ClaimContractAddress =
 //   "0xF142C0Ff42276CfE5bF0dd1A93846FC56d85dE00";
 
-export const ClaimContractAddress =
-  "0x1a2419E43B19D9e51a59a1F16CA9895b773aE388";
+// export const ClaimContractAddress =
+//   "0x1a2419E43B19D9e51a59a1F16CA9895b773aE388";
 
 export const CHAIN_ID = process.env.VUE_APP_CHAIN_ID || 1;
 
--- src/views/Claim-Cun.vue
+++ src/views/Claim-Cun.vue
@@ -20,16 +20,17 @@
                   </v-col>
                 </v-row>
               </v-card-text>
-              <v-divider v-if="state.assets.claimBalance > 0"></v-divider>
+              <v-divider></v-divider>
               <v-card-actions class="justify-center">
                 <v-btn
-                  v-if="state.assets.claimBalance > 0"
                   large
                   color="primary"
                   dark
                   width="80%"
                   @click="claim"
-                  :disabled="state.assets.claimBalance <= 0"
+                  :disabled="
+                    state.assets.claimBalance <= 0 && !state.assets.isClaim
+                  "
                 >
                   {{ $t("Claim") }}
                 </v-btn>
@@ -94,7 +95,6 @@ import WalletConnectProvider from "@walletconnect/web3-provider";
 import { getChainData } from "@/utils/utilities";
 import { getContract, formatAmount } from "@/utils/contract";
 import { CHAIN_ID, NETWORK_ID } from "@/constants";
-import AddressArray from "@/assets/address.json";
 
 const initStats = {
   fetching: false,
@@ -169,22 +169,15 @@ export default {
       const { web3, address } = this.state;
       this.state.fetching = true;
       try {
-        // 检查账号是否在json内，如果内则进行合约查询，不在统一为0
-        let claimAmount = 0;
-        const searchResultArray = AddressArray.filter(item => item == address);
-        if (searchResultArray.length > 0) {
-          // 查询领取额度
-          const ClaimContract = await getContract("Claim", web3);
-          const claimData = await ClaimContract.claimInfoByToken(address);
-          // 如果是提取额度小于等于0，代表未提取，则要显示空投额度
-          if (claimData.claimAmount <= 0) {
-            const tempClaimAmount = await ClaimContract.claimAmount();
-            claimAmount = formatAmount(tempClaimAmount);
-          }
-        }
+        const ERC20Contract = await getContract("ERC20", web3);
+        const ERC20Balance = await ERC20Contract.balanceOf(address);
+        const ClaimContract = await getContract("Claim", web3);
+        const claimData = await ClaimContract.claimInfoByToken(address);
 
         const assets = {
-          claimBalance: claimAmount
+          ERC20Balance: formatAmount(ERC20Balance),
+          claimBalance: formatAmount(claimData.claimAmount),
+          claimStatus: claimData.isClaim
         };
 
         const assetsState = {
